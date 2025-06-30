358bcecb2ffca57ab05e8747f68836610e22a11c
aksoyalimelih <aksoyalimelih@gmail.com>
2025-06-30 23:17:44 +0300

    update

--- server.js
+++ server.js
@@ -76,19 +76,12 @@ function authenticateJWT(req, res, next) {
 }
 
 // Middleware
-app.use(helmet());
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:5175',
-    'http://localhost:5176',
-    'http://localhost:5177',
-    'https://couva.de',
-    'https://www.couva.de' // www'li domain de eklendi
-  ],
-  credentials: true
+  origin: ['https://couva.de', 'http://localhost:5173'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use(helmet());
 app.use(express.json({ limit: '50mb' })); // Resim için daha büyük limit
 app.use(cookieParser());
 app.use(passport.initialize());
